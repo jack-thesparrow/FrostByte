c1c9d49e78461925340b50c79aaf3729325facbc
jack-thesparrow <dummymail@github.com>
2025-05-21 17:01:55 +0530

    optimized xe driver loading on boot

--- system/modules/gpu.nix
+++ system/modules/gpu.nix
@@ -11,34 +11,40 @@
   };
 
   config = lib.mkIf config.xe.driver.enable {
+    # Disable legacy driver
     boot.blacklistedKernelModules = [ "i915" ];
-    boot.initrd.kernelModules = [ "xe" ];
+
+    # Load xe driver later in boot, not in initrd
+    boot.kernelModules = [ "xe" ];
     boot.kernelParams = [ "xe.force_probe=*" ];
+
     services.xserver.enable = false;
     services.xserver.videoDrivers = [ "xe" ];
-    hardware.opengl = {
+
+    # Use updated graphics options
+    hardware.graphics = {
       enable = true;
-      #driSupport = true;
-      driSupport32Bit = true;
+      enable32Bit = true;
+
       extraPackages = with pkgs; [
+        intel-media-driver
+        intel-compute-runtime
+        intel-vaapi-driver
+        libvdpau-va-gl
         vulkan-loader
         vulkan-tools
         mesa
       ];
+
       extraPackages32 = with pkgs; [
         driversi686Linux.intel-media-driver
         driversi686Linux.mesa
       ];
     };
-    hardware.graphics.enable = true;
-    hardware.graphics.enable32Bit = true;
-    hardware.graphics.extraPackages = with pkgs; [
-      intel-media-driver
-      intel-compute-runtime
-      intel-vaapi-driver
-      libvdpau-va-gl
-    ];
+
     environment.systemPackages = with pkgs; [ linux-firmware ];
+
+    # Wayland cursor fix for Intel GPUs
     environment.variables.WLR_NO_HARDWARE_CURSORS = "1";
   };
 }
