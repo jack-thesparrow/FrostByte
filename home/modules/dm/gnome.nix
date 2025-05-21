15ff720180708764336c0d1298122aeaeeb398f8
jack-thesparrow <dummymail@github.com>
2025-05-21 21:48:15 +0530

    added gnome shortcuts

--- home/modules/dm/gnome.nix
+++ home/modules/dm/gnome.nix
@@ -1,26 +1,58 @@
-{ config, pkgs, ... }:
-
 {
-  home.packages = with pkgs; [
-    dconf
-  ];
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
-  dconf.settings = {
-    "org/gnome/settings-daemon/plugins/media-keys" = {
-      custom-keybindings = [
-        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
-      ];
-    };
-
-    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+let
+  customBindings = [
+    {
       name = "Launch Ghostty";
       command = "ghostty";
       binding = "<Super>t";
+    }
+    {
+      name = "Open File Manager";
+      command = "nautilus";
+      binding = "<Super>e";
+    }
+    {
+      name = "Screenshot";
+      command = "gnome-screenshot --interactive";
+      binding = "<Super>p";
+    }
+  ];
+
+  keybindPaths = lib.imap0 (
+    i: _: "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom${toString i}/"
+  ) customBindings;
+
+  keybindSettings = lib.listToAttrs (
+    lib.imap0 (i: binding: {
+      name = "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom${toString i}";
+      value = {
+        name = binding.name;
+        command = binding.command;
+        binding = binding.binding;
+      };
+    }) customBindings
+  );
+
+in
+{
+  home.packages = with pkgs; [
+    dconf
+    gnome-screenshot
+  ];
+
+  dconf.settings = keybindSettings // {
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      custom-keybindings = keybindPaths;
     };
 
     "org/gnome/desktop/wm/keybindings" = {
       close = [ "<Super>q" ];
-      toggle-fullscreen = [ "<Alt>Return" ];
     };
   };
 }
